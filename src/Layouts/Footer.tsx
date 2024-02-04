8996a2e6c591ab6a7347cbeb4888480191e758cc
bmiit145 <21bmiit145@gmail.com>
2024-02-04 11:58:39 +0530

    Credit updated

--- src/Layouts/Footer.tsx
+++ src/Layouts/Footer.tsx
@@ -8,11 +8,11 @@ const Footer = () => {
                 <Container fluid>
                     <Row>
                         <Col sm={6}>
-                            {new Date().getFullYear()} © Velzon.
+                            {new Date().getFullYear()} © Spirit Solutions
                         </Col>
                         <Col sm={6}>
                             <div className="text-sm-end d-none d-sm-block">
-                                Design & Develop by Themesbrand
+                                Design & Develop by Spirit Solutions
                             </div>
                         </Col>
                     </Row>
